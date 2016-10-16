239a1c368439240b1572103581ed8ffcf48b744d
Brad Knowles <code@bradknowles.com>
2016-10-16 16:28:32 -0400

    Add installation script for SlickRun
    SlickRun stores its settings in %AppData%\SlickRun and contains more than
    1 file.  Implemented logic to distinquish between (F)ile links and
    (D)irectory links.
    
    Lots of changes to install_win.cmd to handle hard/file and
    junction/directory links. Will need to clean up this file as it already
    has duplicate code and logic checks.

--- install_win.cmd
+++ install_win.cmd
@@ -1,9 +1,10 @@
 @ECHO OFF
 SetLocal EnableExtensions EnableDelayedExpansion
 
-SET name=%1
-SET source=%2
-SET dest=%3
+SET linktype=%1
+SET name=%2
+SET source=%3
+SET dest=%4
 
 REM ECHO %name%
 REM ECHO %source%
@@ -13,13 +14,14 @@ REM *************************
 REM Set Return Code Constants
 REM *************************
 SET /A SUCCESS=0
+SET /A INVALID_LINK_TYPE=1
 
 ECHO Installing %name%
 ECHO =================
 IF EXIST "%dest%" (
 	SET /P replace=%dest% already exists overwrite? [Y/n] || SET replace=Y
 	IF /I !replace!==y (
-		CALL :DELETE_FILE
+		CALL :DELETE_LINK
 		CALL :MAKE_LINK
 	)
 ) ELSE (
@@ -33,12 +35,27 @@ ECHO ===================
 EndLocal
 EXIT /B %SUCCESS%
 
-:DELETE_FILE
+:DELETE_LINK
 ECHO Deleting %dest%
-DEL "%dest%"
-GOTO :EOF
+IF /I %linktype%==F (
+	DEL "%dest%"
+	GOTO :EOF
+)
+IF /I %linktype%==D (
+	RD /S /Q "%dest%" || ECHO This failed^^!
+	GOTO :EOF
+)
+EXIT /B %INVALID_LINK_TYPE%		
 
 :MAKE_LINK
-mklink /H %dest% %source%
-GOTO :EOF
+IF /I %linktype%==F (
+	mklink /H %dest% %source%
+	GOTO :EOF
+)
+
+IF /I %linktype%==D (
+	mklink /J %dest% %source%
+	GOTO :EOF
+)
+EXIT /B %INVALID_LINK_TYPE%		
 
