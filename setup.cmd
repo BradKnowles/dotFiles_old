99591bd42d9d975d0b9ae2b435a05c721a5bf95d
Brad Knowles <code@bradknowles.com>
2016-10-24 00:31:53 -0400

    Initial commit of vimrc setup
    Updated setup.cmd with command to copy vimrc
    Added a blank vimrc to vim directory

--- setup.cmd
+++ setup.cmd
@@ -6,4 +6,5 @@ SET home=%HOMEDRIVE%%HOMEPATH%
 CLS
 CALL install_win.cmd F .gitconfig git\.gitconfig %home%\.gitconfig
 CALL install_win.cmd D "SlickRun Settings" SlickRun %AppData%\SlickRun
+CALL install_win.cmd f .vimrc vim\.vimrc %home%\.vimrc
 
